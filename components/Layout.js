49010aa03b806e2a27f4be241f99b139891f3662
DevLepsy <70275748+DevLepsy@users.noreply.github.com>
2022-02-09 22:24:33 +0100

    Add Layout  component

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
 import Navbar from "./Navbar";
+import Meta from "./Meta";
+import Footer from "./Footer";
 const Layout = ({ children }) => {
   return (
     <>
+      <Meta />
       <Navbar />
       <main>{children}</main>
+      <Footer />
     </>
   );
 };
